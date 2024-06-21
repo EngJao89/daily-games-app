534e40e9004147a788a971d4345ec94619e2289e
EngJao89 <eng_joao@outlook.com>
2024-06-21 00:29:27 -0300

    feat(dg03-home): implementa tela de home com sugestão de jogos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,40 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Container } from "@radix-ui/themes";
 
-export default function Home() {
+import { GameProps } from "@/utils/types/game";
+
+async function getDalyGame(){
+  try{
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
+    return res.json();
+  }catch(err){
+    throw new Error("Failed to fetch data")
+  }
+}
+
+export default async function Home() {
+  const dalyGame: GameProps = await getDalyGame();
+
   return (
-    <main>
-      <h1>Daily Games</h1>
+    <main className="w-full">
+      <Container className="max-w-screen-xl mx-auto px-3">
+        <h1 className="text-center font-bold text-xl mt-8 mb-5">
+          Separamos um jogo exclusivo pra você
+        </h1>
+        <Link href={`/game/${dalyGame.id}`}>
+          <section className="w-full bg-black rounded-lg">
+            <Image
+              src={dalyGame.image_url}
+              alt={dalyGame.title}
+              priority={true}
+              quality={100}
+              width={100}
+              height={100}
+            />
+          </section>
+        </Link>
+      </Container>
     </main>
   );
 }
